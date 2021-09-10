29bb67f7e915a72ea4ecc87639056d8544754ca2
pyg <qkrdudrbs14@gmail.com>
2021-09-10 17:36:28 +0900

    spring boot mail service

---
+++ src/main/java/com/example/demo/service/MailService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.MailDomain;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+
+    private JavaMailSender mailSender;
+    private static final String FROM_ADDRESS = "YOUR_EMAIL_ADDRESS";
+
+    public void mailSend(MailDomain mailDomain) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailDomain.getAddress());
+        message.setFrom(MailService.FROM_ADDRESS);
+        message.setSubject(mailDomain.getTitle());
+        message.setText(mailDomain.getMessage());
+
+        mailSender.send(message);
+    }
+
+}
